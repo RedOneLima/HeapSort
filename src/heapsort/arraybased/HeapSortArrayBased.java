6af8d35343b1dcba034ae458db86761539ab7ea5
Kyle Hewitt <khewitt08@live.com>
2017-01-21 08:04:59 -0700

    Inital commit

---
+++ src/heapsort/arraybased/HeapSortArrayBased.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heapsort.arraybased;
+
+/**
+ *
+ * @author Kyle
+ */
+public class HeapSortArrayBased {
+    private static int[] a;
+    private static int n;
+    private static int left;
+    private static int right;
+    private static int largest;
+
+    
+    public static void buildheap(int []a){
+        n=a.length-1;
+        for(int i=n/2;i>=0;i--){
+            maxheap(a,i);
+        }
+    }
+    
+    public static void maxheap(int[] a, int i){ 
+        left=2*i;
+        right=2*i+1;
+        if(left <= n && a[left] > a[i]){
+            largest=left;
+        }
+        else{
+            largest=i;
+        }
+        
+        if(right <= n && a[right] > a[largest]){
+            largest=right;
+        }
+        if(largest!=i){
+            exchange(i,largest);
+            maxheap(a, largest);
+        }
+    }
+    
+    public static void exchange(int i, int j){
+        int t=a[i];
+        a[i]=a[j];
+        a[j]=t; 
+        }
+    
+    public static void sort(int []a0){
+        a=a0;
+        buildheap(a);
+        
+        for(int i=n;i>0;i--){
+            exchange(0, i);
+            n=n-1;
+            maxheap(a, 0);
+        }
+    }
+    
+    public static void main(String[] args) {
+        int []a1={570,474,318,264,435,235,314,263,589,190,255,597,593,563,848,749,607,905,979,986};
+        sort(a1);
+        for(int i=0;i<a1.length;i++){
+            System.out.print(a1[i] + " ");
+        }
+    }
+}
